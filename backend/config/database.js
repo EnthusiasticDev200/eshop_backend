0da78ec82c385517e090b9e7bd63fa2d1ad5bb2e
EthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-21 12:31:16 +0100

    modified db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb"
+import { MongoClient, ServerApiVersion } from "mongodb"
 import dotenv from "dotenv"
 
 
@@ -8,29 +8,71 @@ let db = null
 let client = null
 
 export async function connectDb(){
-    const dbName = process.env.NODE_ENV === 'development'
-        ? process.env.ENV_DB_NAME 
-        : process.env.PROD_DB_NAME 
+    if( process.env.NODE_ENV === 'development'){
+        const dbName = process.env.ENV_DB_NAME
+        const uri = process.env.ENV_MONGODB_STRING
 
-    const uri = process.env.NODE_ENV ==='development'
-        ? process.env.ENV_MONGODB_STRING
-        : process.env.PROD_MONGO_STRING
-    try{
-        if (!client){
-            client = new MongoClient(uri)
-            await client.connect()
-            console.log("DB connected successfully")
+        try{
+            if( !client){
+                client = new MongoClient(uri)
+                await client.connect()
+                console.log("DB connected to dev successfully")
+            }
+            if ( !db ){
+                db = client.db(dbName)
+            }
+            return db
+        }catch(error){
+            console.log("error connecting to DB", error.stack)
         }
-        if(!db){
-            db = client.db(dbName)
-            console.log("using database", dbName)
+    }else{
+        const dbName = process.env.PROD_DB_NAME
+        const uri = process.env.PROD_MONGODB_STRING
+        try{
+            if( !client ){
+                client = new MongoClient(uri, {
+                    serverApi : {
+                        version: ServerApiVersion.v1,
+                        strict: true,
+                        deprecationErrors: true,
+                    }
+                })
+            }
+            if ( !db ){
+                db = client.db(dbName)
+                console.log("using database ", db)
+            }
+            return db
+        }catch(error){
+            console.log("error connecting to prod instance", error.stack)
         }
-        return db
-    }catch(error){
-        console.error("DB connection failed", error)
     }
-    
 }
+
+// export async function connectDb(){
+//     const dbName = process.env.NODE_ENV === 'development'
+//         ? process.env.ENV_DB_NAME 
+//         : process.env.PROD_DB_NAME 
+
+//     const uri = process.env.NODE_ENV ==='development'
+//         ? process.env.ENV_MONGODB_STRING
+//         : process.env.PROD_MONGO_STRING
+//     try{
+//         if (!client){
+//             client = new MongoClient(uri)
+//             await client.connect()
+//             console.log("DB connected successfully")
+//         }
+//         if(!db){
+//             db = client.db(dbName)
+//             console.log("using database", dbName)
+//         }
+//         return db
+//     }catch(error){
+//         console.error("DB connection failed", error)
+//     }
+    
+// }
 
 
 // create collection
